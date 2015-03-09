701a618e2fe40e143647502d6dedc8099cb58715
Neil Rayborn <neilrayborn@Neils-MBP.digium.internal>
2015-03-09 16:37:41 -0500

    functional tests

--- test/functional/ticTacToe.spec.js
+++ test/functional/ticTacToe.spec.js
@@ -4,6 +4,41 @@ var webdriverio = require('webdriverio');
 var enableDestroy = require('server-destroy');
 var http = require('http');
 var server = require('../../server/server');
+var async = require('async')
+var assert = require('assert')
+var closeBrowsers = true;
+var environment = 'local';
+
+function getBrowser() {
+    var options;
+    if (environment === 'local') {
+        options = { browserName: 'chrome' };
+    } else if (environment === 'browserstack') {
+        options = {
+            host: 'hub.browserstack.com',
+            port: 80,
+            user: process.env.BROWSERSTACK_USERNAME,
+            key: process.env.BROWSERSTACK_ACCESS_KEY,
+            logLevel: 'silent',
+            desiredCapabilities: {
+                browser: 'chrome',
+                browser_version: '38.0',
+                os: 'OS X',
+                os_version: 'Mavericks',
+                resolution: '1280x1024',
+                args: [ 'incognito' ]
+            }            
+        };
+    }
+ 
+    return webdriverio.remote(options).addCommand('finish', function (cb) {
+        if (closeBrowsers) {
+            this.end(cb);
+            return;
+        }
+        cb();
+    });
+}
 
 describe("tic tac toe", function () {
     this.timeout(30 * 1000);
@@ -15,17 +50,14 @@ describe("tic tac toe", function () {
         httpServer = http.createServer(server);
         enableDestroy(httpServer);
         httpServer.listen(5678, function () {
-
             // instantiate webdriver client
-            client = webdriverio.remote({
-                desiredCapabilities: { browserName: 'chrome' }
-            }).init(done);
+            client = getBrowser().init(done);
         });
     });
 
     after(function (done) {
         // close webdriver browser
-        client.end(function () {
+        client.finish(function () {
 
             // stop static site
             httpServer.destroy(done);
@@ -36,9 +68,104 @@ describe("tic tac toe", function () {
 
         it("shows logout button", function (done) {
             client.url('http://localhost:5678/modules/tic-tac-toe/index.html')
-                .setValue('div#login input', 'bob')
-                .click('div#login button')
-                .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, done);
+                    .setValue('div#login input', 'bob')
+                    .click('div#login button')
+                    .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, done);
         });
     });
+
+    describe("with two clients connected", function () {
+        var opponent;
+
+        before(function (done) {
+
+            opponent = getBrowser().init(function () {
+
+                async.parallel([
+                    function (next) {
+                        client.url('http://localhost:5678/modules/tic-tac-toe/index.html')
+                          .setValue('div#login input', 'client')
+                          .click('div#login button')
+                          .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, next);
+                        },
+                    function (next) {
+                        opponent.url('http://localhost:5678/modules/tic-tac-toe/index.html')
+                            .setValue('div#login input', 'opponent')
+                            .click('div#login button')
+                            .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, next);
+                        }
+                ], done);
+            });
+        });
+
+        after(function (done) {
+            // close webdriver browser
+            opponent.finish(done)
+        });
+        
+        it("has an opponent available", function (done) {
+            client.waitForExist('//td[contains(text(), "opponent")]', 3000, done)
+        });
+        
+       it("can challenge opponent to a match of tic tac toe", function (done) {
+           client.waitForExist('//td[contains(text(), "opponent")]', 2000).click('//td[contains(text(), "opponent")]', function(){
+               opponent.waitForExist('div[data-selenium-hook="accept-invitation-client"]', 2000).click('div[data-selenium-hook="accept-invitation-client"]', function(){
+                   client.waitForExist('table[data-selenium-hook="game-board-opponent"]', 2000, function () {
+                       opponent.waitForExist('table[data-selenium-hook="game-board-opponent"]', 2000, done);
+                   });                   
+               })
+           });
+       })
+    });
+    
+    describe("with two clients playing", function () {
+        var opponent;
+
+        before(function (done) {
+
+            opponent = getBrowser().init(function () {
+
+                async.parallel([
+                    function (next) {
+                        client.url('http://localhost:5678/modules/tic-tac-toe/index.html')
+                          .setValue('div#login input', 'client')
+                          .click('div#login button')
+                          .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, next);
+                        },
+                    function (next) {
+                        opponent.url('http://localhost:5678/modules/tic-tac-toe/index.html')
+                            .setValue('div#login input', 'opponent')
+                            .click('div#login button')
+                            .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, next);
+                        }
+                ], function (err) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+                    
+                    client.waitForExist('//td[contains(text(), "opponent")]', 2000).click('//td[contains(text(), "opponent")]', function(){
+                        opponent.waitForExist('div[data-selenium-hook="accept-invitation-client"]', 2000).click('div[data-selenium-hook="accept-invitation-client"]', function(){
+                            client.waitForExist('table[data-selenium-hook="game-board-opponent"]', 2000, function () {
+                                opponent.waitForExist('table[data-selenium-hook="game-board-opponent"]', 2000, done);
+                            });                   
+                        })
+                    });                    
+                });
+            });
+        });
+
+        after(function (done) {
+            // close webdriver browser
+            opponent.finish(done)
+        });
+        
+        it('allows the client to quit', function (done) {           
+            client.click("button[data-selenium-hook='quit-button']")
+                    .waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, function(){
+                        opponent.alertAccept().waitForExist('button[data-selenium-hook="logout"]', 2 * 1000, done);
+                })
+        });
+    });
+    
 });
